b61358ed62fbe843ffdb6c2a76f4b877480f65f8
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2025-08-01 00:11:14 -0500

    update resume

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,7 @@ export default function Home() {
           <p className="text-lg leading-relaxed">
             I’m a mission‑driven Software Engineer with 8+ years of full‑stack experience leveraging software
             to create effective platforms for diverse populations. Skilled in accessibility, picking up
-            new tools, cloud infrastructure and building scalable services for millions of users. I’ve built
+            new tools, and building scalable services for millions of users. I’ve built
             inclusive digital platforms mainly for government agencies and nonprofits.
           </p>
           <p className="text-lg leading-relaxed">
