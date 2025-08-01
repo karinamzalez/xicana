bf6f4f5b1e980b2de612da45953a1d21243d74e2
Karina Munoz Gonzalez <karinamzalez@gmail.com>
2025-07-31 23:37:27 -0500

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -218,7 +218,7 @@ export default function Home() {
             />
           </div>
           <p className="text-lg leading-relaxed">
-            crbn ftprnt is an installation I designed and built. All code was written in Go and JavaScript. I collaborated with my software collective, <a href="https://github.com/bright-wwweb/crbn-ftprnt" className="text-accent hover:underline">bright.wwweb</a>. The installation
+            crbn ftprnt is an installation I designed and built. All code was written in Go and TypeScript. I collaborated with my software collective, <a href="https://github.com/bright-wwweb/crbn-ftprnt" className="text-accent hover:underline">bright.wwweb</a>. The installation
             was commissioned by <a href="http://www.warmcookiesoftherevolution.org/" className="text-accent hover:underline">Warm Cookies Revolution</a> for an event
             centered around "energy". I chose to focus on the energy we consume, our carbon footprints. What resulted was an immersive experience that enabled people to interact visual representation of our collective carbon footprint.
           </p>
